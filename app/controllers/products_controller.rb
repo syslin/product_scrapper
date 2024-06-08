5dbde7ec6e34e31b39ca150b7889a5887c4d674c
sanima <saneemastha@gmail.com>
2024-06-08 14:56:41 +0545

    feat: service added for scrape

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def create
+    url = params[:url]
+    ScraperService.new(url).scrape
+    render json: { message: 'Product scraped successfully' }
+  end
+
+  def update
+    product = Product.find(params[:id])
+    ScraperService.new(product.url).scrape
+    render json: { message: 'Product updated successfully' }
+  end
 end
---
+++ app/services/scraper_service.rb
@@ -0,0 +1,41 @@
+require 'open-uri'
+require 'nokogiri'
+
+class ScraperService
+  USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+  def initialize(url)
+    @url = url
+  end
+
+  def scrape
+    retry_count = 0
+    begin
+      doc = Nokogiri::HTML(URI.open(@url, 'User-Agent' => USER_AGENT))
+      # Extract product details
+      title = doc.css('title').text.strip
+      description = doc.css('meta[name="og:description"]').first['content']
+      parsed_data = JSON.parse(doc.css('script').text.match(/pdpTrackingData\s*=\s*"(.*?)";/m)[1].gsub('\\"', '"').gsub('\\\\', '\\'))
+      price = parsed_data['pdt_price'].gsub(/\D/, '').to_i
+      contact_info =  parsed_data['seller_name']
+      category_name = parsed_data['pdt_category'][0]
+
+      category = Category.find_or_create_by(name: category_name)
+
+      Product.create!(
+        title: title,
+        description: description,
+        price: price,
+        category: category
+      )
+    rescue OpenURI::HTTPError => e
+      if retry_count < MAX_RETRIES
+        retry_count += 1
+        sleep 2 ** retry_count # Exponential backoff
+        retry
+      else
+        raise "Failed to scrape the URL after #{MAX_RETRIES} attempts: #{e.message}"
+      end
+    end
+  end
+end
